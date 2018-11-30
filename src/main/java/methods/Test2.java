c4906919807d4a497580f89f41430dd2c9c43c1d
Justin Rowles <justin.rowles@gmail.com>
2018-11-30 14:58:19 +0000

    Change message (testing)

--- src/main/java/methods/Test2.java
+++ src/main/java/methods/Test2.java
@@ -12,7 +12,7 @@ public class Test2  implements Method {
 	@Override
 	public void handle(JSONObject event, JSONObject responseBody) {
 
-	    String greeting = "Apples";
+	    String greeting = "Green Apples";
 	    
 	    responseBody.put("input", event);
 	    responseBody.put("message", greeting);

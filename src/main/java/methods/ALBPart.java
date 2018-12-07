1abd0386cd87c5e260916c4481a8ea4ad4df3894
Justin Rowles <justin.rowles@gmail.com>
2018-12-07 14:58:56 +0000

    Correct format for alb part

--- src/main/java/methods/ALBPart.java
+++ src/main/java/methods/ALBPart.java
@@ -16,14 +16,22 @@ public class ALBPart implements Method {
 	@Override
 	public void handle(JSONObject event, Map<String, Object> response) {
 
+		response.put("input", event);
+		
+		response.put("isBase64Encoded", false);
+		response.put("statusCode", 200);
+		response.put("statusDescription", "200 OK");
+		Map<String, Object> headers = new HashMap<>();
+		//headers.put("Set-cookie", "cookies");
+		headers.put("Content-Type", "application/json");
+		response.put("headers", new JSONObject(headers)); 
+
 		Map<String, Object> body = new HashMap<>();
 	    body.put("message", greeting);
 	    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());		
 	    body.put("date", timeStamp);
 
-	    response.put("body", body);
-	    response.put("input", event);
-	    response.put("statusCode", "400");
+	    response.put("body", new JSONObject(body));
 	}
 
 }
